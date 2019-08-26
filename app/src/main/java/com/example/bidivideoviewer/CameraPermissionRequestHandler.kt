3da9686e6a77199418bfc307b45badad558baace
LewisCollum <lewiscollum4@gmail.com>
2019-08-26 03:28:23 -0400

    Untabified

--- app/src/main/java/com/example/bidivideoviewer/CameraPermissionRequestHandler.kt
+++ app/src/main/java/com/example/bidivideoviewer/CameraPermissionRequestHandler.kt
@@ -3,34 +3,34 @@ package com.example.bidivideoviewer
 import android.content.pm.PackageManager
 
 class CameraPermissionRequestHandler {
-	companion object {
-		private var onPermissionGrantedListener: () -> Unit = {}
-		var isPermissionGranted: Boolean = false
-			private set
-		
-		fun setOnPermissionGrantedListener(listener: () -> Unit) {
-			onPermissionGrantedListener = listener
-		}
-				
-		fun handleWithGrantResults(grantResults: IntArray) {
-			val doGrantResultsExceedLength: Boolean = grantResults.size != 1
-			val isPermissionDenied: Boolean = grantResults[0] != PackageManager.PERMISSION_GRANTED
-			
-			if (doGrantResultsExceedLength || isPermissionDenied) {
-				handleCameraPermissionDenied()
-			} else {
-				handleCameraPermissionGranted()
-			}
-		}
-		
-		private fun handleCameraPermissionDenied() {
-			//ErrorDialog.newInstance(getString(R.string.request_permission)).show(getChildFragmentManager(), FRAGMENT_DIALOG)
-		}
+    companion object {
+        private var onPermissionGrantedListener: () -> Unit = {}
+        var isPermissionGranted: Boolean = false
+            private set
+        
+        fun setOnPermissionGrantedListener(listener: () -> Unit) {
+            onPermissionGrantedListener = listener
+        }
+                
+        fun handleWithGrantResults(grantResults: IntArray) {
+            val doGrantResultsExceedLength: Boolean = grantResults.size != 1
+            val isPermissionDenied: Boolean = grantResults[0] != PackageManager.PERMISSION_GRANTED
+            
+            if (doGrantResultsExceedLength || isPermissionDenied) {
+                handleCameraPermissionDenied()
+            } else {
+                handleCameraPermissionGranted()
+            }
+        }
+        
+        private fun handleCameraPermissionDenied() {
+            //ErrorDialog.newInstance(getString(R.string.request_permission)).show(getChildFragmentManager(), FRAGMENT_DIALOG)
+        }
 
-		private fun handleCameraPermissionGranted() {
-			onPermissionGrantedListener()
-			isPermissionGranted = true
-		}
-	}
+        private fun handleCameraPermissionGranted() {
+            onPermissionGrantedListener()
+            isPermissionGranted = true
+        }
+    }
 }
 
